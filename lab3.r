8b987891f0bd92b57a260b01c060490d1ed0b30c
Nico <nicolas.romero.f@usach.cl>
2019-01-02 21:37:47 -0300

    se hacen reglas

--- lab3.r
+++ lab3.r
@@ -2,8 +2,8 @@ library(arulesViz)
 library(dplyr)
 library(rgl)
 
-#path = "F:/Google Drive/USACH/Nivel 8/Analisis de datos/lab2/hepatitis.data"
-path = "~/Documentos/AnalisisDatosLab3/hepatitis.data"
+path = "F:/Google Drive/USACH/Nivel 8/Analisis de datos/lab3/hepatitis.data"
+#path = "~/Documentos/AnalisisDatosLab3/hepatitis.data"
 hepatitis <- read.table(path,sep=",", na.strings = c("?"))
 
 names <- c("CLASS","AGE","SEX","STEROID","ANTIVIRALS","FATIGUE","MALAISE",
@@ -14,8 +14,7 @@ names <- c("CLASS","AGE","SEX","STEROID","ANTIVIRALS","FATIGUE","MALAISE",
 colnames(hepatitis) <- names
 
 hepatitis.without.na <- na.omit(hepatitis)
-
-data = select(hepatitis.without.na,-CLASS)
+data = hepatitis.without.na
 data[["SEX"]] = factor(data[["SEX"]])
 data[["STEROID"]] = factor(data[["STEROID"]])
 data[["ANTIVIRALS"]] = factor(data[["ANTIVIRALS"]])
@@ -29,5 +28,24 @@ data[["SPIDERS"]] = factor(data[["SPIDERS"]])
 data[["ASCITES"]] = factor(data[["ASCITES"]])
 data[["VARICES"]] = factor(data[["VARICES"]])
 data[["HISTOLOGY"]] = factor(data[["HISTOLOGY"]])
+data[["CLASS"]] = factor(data[["CLASS"]])
 
-ruleExplorer(data)
+# 
+# frequentItems <- eclat (data, parameter = list(supp = 0.2, minlen=3, maxlen = 4))
+# inspect(frequentItems)
+
+
+
+rules <- apriori(data, parameter=list(support=0.2, confidence=0.8, minlen=3, maxlen=6))
+rules
+subsetRules <- which(colSums(is.subset(rules, rules)) > 1) # get subset rules in vector
+length(subsetRules)  #> 3913
+rules <- rules[-subsetRules] # remove subset rules.
+
+rules_conf <- sort (rules, by="confidence", decreasing=TRUE) # 'high-confidence' rules.
+inspect(head(rules_conf))
+
+rules_lift <- sort (rules, by="lift", decreasing=TRUE) # 'high-lift' rules.
+inspect(head(rules_lift))
+a = ruleExplorer(rules)
+print(a)
